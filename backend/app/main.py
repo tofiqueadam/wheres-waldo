4d2651b0fae0fb17ff07567e80c3484bb2589a69
Tofique <tofiqueadam@gmail.com>
2026-09-08 14:49:39 +0300

    Register score API

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,11 +3,16 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.database import Base, engine
 from app.routers import characters_router
 from app.models.image import Image
+from app.models.score import Score
+from app.routers.game import router as game_router
+from app.routers.scores import router as score_router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.include_router(game_router)
+app.include_router(score_router)
 
 app.add_middleware(
     CORSMiddleware,
